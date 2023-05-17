930b85d70f17bb37d5f862c8287feed8f1a9c6bc
piper93 <49284223+piper93@users.noreply.github.com>
2023-05-17 09:25:15 +0000

    Updated with OoA finale

--- js/timeline.js
+++ js/timeline.js
@@ -33,7 +33,7 @@ class Character {
 }
 
 const CHARACTERS = [
- new Character("bl","Bask",true,"img/bl/ginness.webp"),
+ new Character("bl","Bask",true,"img/bl/bask.webp"),
  new Character("bl","Ginness",true,"img/bl/ginness.webp"),
  new Character("bl","Kiya",true,"img/bl/kiya.webp"),
  new Character("bl","Shade", true, "img/bl/shade.webp"),
@@ -228,7 +228,9 @@ const EVENTS = [
  new Event("ooa","Met with Loy Ayton; The Slugs of the Waste assaulted the Gunworks; The Maw of Rovagug was destroyed by Pyronite; Yohziah was killed by Vewslog, the Cyclops",4722,6,10),
  new Event("bl","Organised transport to Pagked; Met Bask Vishky",4722,9,20),
  new Event("bl","Arrived in Pagked; Started the search for Decrosia; Bask made a deal with The Shroud; Nebumi began the return trip to Yled alone",4722,9,23),
- new Event("gw","The night of the Missing Moment; hundred of onlookers walk through <i>aiudara</i>",4722,7,21)
+ //new Event("gw","The night of the Missing Moment; hundred of onlookers walk through <i>aiudara</i>",4722,7,21)
+ new Event("ooa","The <i>Gearsmoke</i> Steamboat explodes at the top of Alken Falls, destroying the waterfall and the Tinwound Hydroforge",4722,6,13),
+ new Event("ooa","The High Parliament rushes through Tombend's bill of mandatory conscription; Loveless is placed in control of the new standing army",4722,6,17)
 ]
 
 var sortedEvents = {};
