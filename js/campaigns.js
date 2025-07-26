1eb0bdfe31123290421b2c41dffe4b3be3e74723
piper93 <49284223+piper93@users.noreply.github.com>
2025-07-26 20:06:05 +0000

    Typos

--- js/campaigns.js
+++ js/campaigns.js
@@ -5,7 +5,7 @@ import { AVCHARACTERS } from "./campaigns/av.js"
 import { OOACHARACTERS } from "./campaigns/ooa.js"
 import { BLCHARACTERS } from "./campaigns/bl.js"
 import { SOGCHARACTERS } from "./campaigns/sog.js";
-import { SWCHARACTERS } from "/campaigns.sw.js";
+import { SWCHARACTERS } from "./campaigns/sw.js";
 
 export const CHARACTERS = AOACHARACTERS.concat(ECCHARACTERS, NOTGDCHARACTERS, AVCHARACTERS, 
   OOACHARACTERS, BLCHARACTERS, SOGCHARACTERS, SWCHARACTERS)
@@ -17,7 +17,7 @@ import { ABOMINATIONVAULTS } from "./campaigns/av.js"
 import { OUTLAWSOFALKENSTAR } from "./campaigns/ooa.js"
 import { BLOODLORDS } from "./campaigns/bl.js"
 import { SEASONOFGHOSTS } from "./campaigns/sog.js";
-import { SPOREWAR } from "/campaigns/sw.js";
+import { SPOREWAR } from "./campaigns/sw.js";
 
 export const EVENTS = AGEOFASHES.concat(EXTINCTIONCURSE, NIGHTOFTHEGRAYDEATH, ABOMINATIONVAULTS, 
   OUTLAWSOFALKENSTAR, BLOODLORDS, SEASONOFGHOSTS, SPOREWAR)
